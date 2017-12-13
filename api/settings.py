b8a254933a9dcd5f7654de68cfdfab51ea3ca508
Danny Rego <danny@rego.com>
2017-12-12 20:50:42 -0500

    The first real 3d object added.  A simple keychain.  Things are starting to look good.

--- api/settings.py
+++ api/settings.py
@@ -1,11 +1,13 @@
 import os
 
 BACKGROUND_FILE_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'background_models')
-SAMPLE_FILE_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'samples')
+SAMPLE_STL_FILE_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'samples', 'stl')
+SAMPLE_PNG_FILE_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'samples', 'png')
 
 BACKGROUND_FILE_ATTRIBUTES = {
     # NOTE: All units below are in mm unless otherwise noted
     '_DEFAULT': {
+        'display_name': '(none)',
         'description': '(none)',
         'tags': [],
         # QR drawing area bounding cube
@@ -23,9 +25,13 @@ BACKGROUND_FILE_ATTRIBUTES = {
         'no_draw_y': 0,
         # Height at which the instructions should show to change filament
         'change_filament_height': 0,
+        # Any notes to display for this model...
+        'notes': [],
     },
+
     '30mm_keychain': {
-        'description': '3cm keychain',
+        'display_name': '3cm keychain',
+        'description': 'A basic 3cm x 3cm keychain.  Nothing fancy.',
         'tags': ['keychain'],
         'qr_size_x': 24,
         'qr_size_y': 24,
@@ -34,79 +40,90 @@ BACKGROUND_FILE_ATTRIBUTES = {
         'no_draw_x': 3,
         'no_draw_y': 3,
         'change_filament_height': 1.6,
+        'notes': [
+        ],
+
     },
-    '35mm_keychain_with_nfc': {
-        'description': '3.5cm keychain (nfc snap-together)',
-        'tags': ['keychain','nfc'],
-        'qr_size_x': 29,
-        'qr_size_y': 29,
-        'qr_size_z': 1.75,
-        'no_draw_position': 'bottomright',
-        'no_draw_x': 4,
-        'no_draw_y': 4,
-        'change_filament_height': 2.6,
-    },
-    # XXX - Not really finalized from here down
-    '5cm_coaster_no_nfc': {
-        'description': '5cm QR coaster (no NFC)',
-        'tags': ['coaster'],
-        'qr_size_x': 40,
-        'qr_size_y': 40,
-        'qr_size_z': 5,
-        'change_filament_height': 3.0,
-    },
-    '5cm_coaster_nfc': {
-        'description': '5cm QR coaster (with NFC)',
-        'tags': ['coaster'],
-        'qr_size_x': 40,
-        'qr_size_y': 40,
-        'qr_size_z': 5,
-        'no_draw_position': 'bottomright',
-        'no_draw_x': 10,
-        'no_draw_y': 12,
-        'change_filament_height': 3.0,
-    },
-    '5cm_coaster_nfc_no_logo': {
-        'description': '5cm QR coaster (with NFC - no logo)',
-        'tags': ['coaster'],
-        'qr_size_x': 40,
-        'qr_size_y': 40,
-        'qr_size_z': 5,
-        'change_filament_height': 3.0,
-    },
-    # XXX - List to add
-    # 5cm refrigerator magnet with NFC
-    # 5cm refrigerator magnet with NFC (no logo)
-    # 5cm refrigerator magnet with no NFC
-    # XXX - From here down are temporary samples to play with
-    'QRSurround (9cm)': {
-        'description': 'Standard blah blah blah',
-        'qr_size_x': 80,
-        'qr_size_y': 80,
-        'qr_size_z': 3,
-        'no_draw_position': 'center',
-        'no_draw_x': 35,
-        'no_draw_y': 35,
-        'change_filament_height': 3.1,
-    },
-    'QRSurround (10cm)': {
-        'description': 'Standard blah blah blah',
-        'qr_size_x': 100,
-        'qr_size_y': 100,
-        'qr_size_z': 3,
-        'no_draw_position': 'center',
-        'no_draw_x': 35,
-        'no_draw_y': 35,
-        'change_filament_height': 3.1,
-    },
-    'QRSurround (12cm)': {
-        'description': 'Standard blah blah blah',
-        'qr_size_x': 100,
-        'qr_size_y': 100,
-        'qr_size_z': 3,
-        'no_draw_position': 'center',
-        'no_draw_x': 35,
-        'no_draw_y': 35,
-        'change_filament_height': 3.1,
-    },
+    # TODO: From here down have not quite been vetted yet... still working on them.  Move above as they become "official".
+    # '35mm_keychain_with_nfc': {
+    #     'display_name': '',
+    #     'description': '3.5cm keychain (nfc snap-together)',
+    #     'tags': ['keychain','nfc'],
+    #     'qr_size_x': 29,
+    #     'qr_size_y': 29,
+    #     'qr_size_z': 1.75,
+    #     'no_draw_position': 'bottomright',
+    #     'no_draw_x': 4,
+    #     'no_draw_y': 4,
+    #     'change_filament_height': 2.6,
+    # },
+    # # XXX - Not really finalized from here down
+    # '5cm_coaster_no_nfc': {
+    #     'display_name': '',
+    #     'description': '5cm QR coaster (no NFC)',
+    #     'tags': ['coaster'],
+    #     'qr_size_x': 40,
+    #     'qr_size_y': 40,
+    #     'qr_size_z': 5,
+    #     'change_filament_height': 3.0,
+    # },
+    # '5cm_coaster_nfc': {
+    #     'display_name': '',
+    #     'description': '5cm QR coaster (with NFC)',
+    #     'tags': ['coaster'],
+    #     'qr_size_x': 40,
+    #     'qr_size_y': 40,
+    #     'qr_size_z': 5,
+    #     'no_draw_position': 'bottomright',
+    #     'no_draw_x': 10,
+    #     'no_draw_y': 12,
+    #     'change_filament_height': 3.0,
+    # },
+    # '5cm_coaster_nfc_no_logo': {
+    #     'display_name': '',
+    #     'description': '5cm QR coaster (with NFC - no logo)',
+    #     'tags': ['coaster'],
+    #     'qr_size_x': 40,
+    #     'qr_size_y': 40,
+    #     'qr_size_z': 5,
+    #     'change_filament_height': 3.0,
+    # },
+    # # TODO: Add these...
+    # # 5cm refrigerator magnet with NFC
+    # # 5cm refrigerator magnet with NFC (no logo)
+    # # 5cm refrigerator magnet with no NFC
+    # # XXX - From here down are temporary samples to play with
+    # 'QRSurround (9cm)': {
+    #     'display_name': '',
+    #     'description': 'Standard blah blah blah',
+    #     'qr_size_x': 80,
+    #     'qr_size_y': 80,
+    #     'qr_size_z': 3,
+    #     'no_draw_position': 'center',
+    #     'no_draw_x': 35,
+    #     'no_draw_y': 35,
+    #     'change_filament_height': 3.1,
+    # },
+    # 'QRSurround (10cm)': {
+    #     'display_name': '',
+    #     'description': 'Standard blah blah blah',
+    #     'qr_size_x': 100,
+    #     'qr_size_y': 100,
+    #     'qr_size_z': 3,
+    #     'no_draw_position': 'center',
+    #     'no_draw_x': 35,
+    #     'no_draw_y': 35,
+    #     'change_filament_height': 3.1,
+    # },
+    # 'QRSurround (12cm)': {
+    #     'display_name': '',
+    #     'description': 'Standard blah blah blah',
+    #     'qr_size_x': 100,
+    #     'qr_size_y': 100,
+    #     'qr_size_z': 3,
+    #     'no_draw_position': 'center',
+    #     'no_draw_x': 35,
+    #     'no_draw_y': 35,
+    #     'change_filament_height': 3.1,
+    # },
 }
